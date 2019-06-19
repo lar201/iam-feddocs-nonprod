eec210b7b6c46c5c680078885d231d9d4b62255a
xdong68 <x-dong@northwestern.edu>
2019-06-19 09:30:34 -0500

    ENH: adding script

---
+++ qa/deploy-all.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+  
+#SERVERS='evfed312qab.ci.northwestern.edu chfed312qaa.ci.northwestern.edu chfed312qab.ci.northwestern.edu'
+SERVERS=$1 
+RESTARTSERVICE=/home/tomcat/restart-service.sh
+WORKINGDIR=/home/tomcat/iam-feddocs-nonprod/qa/metadata/
+WORKINGDIRSUB=/home/tomcat/iam-feddocs-nonprod/qa/idp-v3/
+METADIR=/usr/local/shibboleth-idpv3/metadata
+CONFDIR=/usr/local/shibboleth-idpv3/conf
+
+echo "servers=$SERVERS"
+if [ -z "$SERVERS" ]; then 
+    echo "no remote server is specified"
+fi
+
+for server in $SERVERS
+do
+        echo ""
+        echo "Deploying on remote server $server"
+
+	echo "copy the directory $WORKINGDIR to $METADIR"
+	scp $WORKINGDIR/* tomcat@$server:$METADIR
+
+	echo "copy the directory $WORKINGDIRSUB to $CONFDIR"
+	scp $WORKINGDIRSUB/* tomcat@$server:$CONFDIR
+
+	echo "restart services on $server"
+	ssh -t tomcat@$server "$RESTARTSERVICE"
+done
